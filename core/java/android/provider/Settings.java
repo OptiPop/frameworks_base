033e25ce85a3f4097ebdd5c541d75639aa75c64a
Aaron Echols <atechols@gmail.com>
2014-11-28 18:39:26 -0500

    framework: port IME selector notification toggle (2/2)
    Change-Id: Ia35960d88468dba44cdfad26c7cc74f811a6844f
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/config.xml
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2166,6 +2166,12 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+ 	 * Whether to show the IME switcher in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
+
+        /**
 	 Deprecated Use {@link android.provider.Settings.Secure.QS_TILES}
          * @hide
          */
