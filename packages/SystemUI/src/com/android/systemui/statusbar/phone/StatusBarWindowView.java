2ef45ed18391838eb6716336c9a5a72009a3d92d
snak3ater <saeedyasir91@gmail.com>
2014-11-30 18:29:56 +0500

    Quick pull down and double tap to sleep gesture: Disable as default
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    
    Change-Id: Id6b37fd0649721e41c5fc7143ed45c35723c2c3c
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -322,7 +322,7 @@ public class StatusBarWindowView extends FrameLayout {
         public void update() {
             ContentResolver resolver = mContext.getContentResolver();
             mDoubleTapToSleepEnabled = Settings.System.getInt(
-                    resolver, Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 1) == 1;
+                    resolver, Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0) == 1;
         }
     }
 }
