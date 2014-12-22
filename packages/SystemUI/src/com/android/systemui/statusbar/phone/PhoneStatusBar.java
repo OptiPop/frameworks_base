718ca0fba562a9ed90b81a87e61b4747857a600d
BigBrother1984 <carlosavignano@aospa.co>
2014-12-22 21:40:21 +0100

    SystemUI: This TODO can be solved actually
    Signed-off-by: BigBrother1984 <carlosavignano@aospa.co>
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Change-Id: I46453e2f0bf9f980fabc566f95445e776512eaec

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1689,9 +1689,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     private void handleUpdateNotifications() {
-        // TODO: Move this into updateNotificationIcons()?
-        if (mNotificationIcons == null) return;
-
         mNotificationData.filterAndSort();
 
         updateNotificationShade();
@@ -1706,6 +1703,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     private void updateNotificationIcons() {
+        if (mNotificationIcons == null) return;
+
         final LinearLayout.LayoutParams params
             = new LinearLayout.LayoutParams(mIconSize + 2*mIconHPadding, mNaturalBarHeight);
 
