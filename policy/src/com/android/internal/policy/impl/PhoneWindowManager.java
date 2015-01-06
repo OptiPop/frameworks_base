1c5ac3016d91d17452d54d31ce9ea34aa2d86b56
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-01-06 01:57:32 +0100

    BootMsgDialog: Use Theme.Material.Dialog.Alert.DarkKat
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5614,7 +5614,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             PackageManager.FEATURE_TELEVISION)) {
                         theme = com.android.internal.R.style.Theme_Leanback_Dialog_Alert;
                     } else {
-                        theme = 0;
+                        theme = 6; // Theme.Material.Dialog.Alert.DarkKat
                     }
 
                     mBootMsgDialog = new ProgressDialog(mContext, theme) {
