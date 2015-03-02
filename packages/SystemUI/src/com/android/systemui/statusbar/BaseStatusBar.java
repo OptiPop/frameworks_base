8ac311d48b65b366ef1dba4bf9bbd1a62851ce24
Adnan Begovic <adnan@cyngn.com>
2015-03-02 10:05:45 +0000

    Base: don't show headsup if IME is showing
    taken from this commit https://github.com/Mahdi-Rom/android_frameworks_base/commit/18294da23b44fa2c24f3759e970f1ee5ac9fedc9
    
    Change-Id: I9edb1b74c0ee5c38d5e82f0ade0ecd7221dfa5a2
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2183,8 +2183,9 @@ public abstract class BaseStatusBar extends SystemUI implements
                 && !accessibilityForcesLaunch
                 && mPowerManager.isScreenOn()
     	        && !isIMEShowing
-                && !keyguardIsShowing;
-                && !isExpanded;
+                && !keyguardIsShowing
+                && !isExpanded
+                && !zenBlocksHeadsUp;
 
         try {
             interrupt = interrupt && !mDreamManager.isDreaming();
@@ -2194,7 +2195,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         // its below our threshold priority, we might want to always display
         // notifications from certain apps
-        if (!isHighPriority && !isOngoing && !keyguardIsShowing && !isExpanded) {
+        if (!isHighPriority && !isOngoing && !keyguardIsShowing && !isExpanded && !zenBlocksHeadsUp && !isIMEShowing) {
             // However, we don't want to interrupt if we're in an application that is
             // in Do Not Disturb
             if (!isPackageInDnd(getTopLevelPackage())) {
