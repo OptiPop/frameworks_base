8cebe42fe45aea4927ba5f9fb7dc03006eddadf0
Craig Gomez <craigacgomez@gmail.com>
2015-03-12 17:26:27 -0700

    Fix FABulous recents clear all

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -41,6 +41,7 @@ import com.android.systemui.recents.model.RecentsTaskLoader;
 import com.android.systemui.recents.model.Task;
 import com.android.systemui.recents.model.TaskStack;
 
+import com.android.systemui.R;
 import java.util.ArrayList;
 
 /**
@@ -66,6 +67,7 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
     ArrayList<TaskStack> mStacks;
     View mSearchBar;
     RecentsViewCallbacks mCb;
+    View mClearRecents;
 	View mFloatingButton;
 
     public RecentsView(Context context) {
@@ -151,6 +153,17 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         // Trigger a new layout
         requestLayout();
     }
+
+	public void dismissAllTasksAnimated() {
+		int childCount = getChildCount();
+		for (int i = 0; i < childCount; i++) {
+			View child = getChildAt(i);
+			if (child != mSearchBar) {
+				TaskStackView stackView = (TaskStackView) child;
+				stackView.dismissAllTasks();
+			}
+		}
+	}
 
     /** Launches the focused task from the first stack if possible */
     public boolean launchFocusedTask() {
@@ -295,7 +308,7 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
             FrameLayout.LayoutParams params = (FrameLayout.LayoutParams)
                     mFloatingButton.getLayoutParams();
             params.topMargin = taskStackBounds.top;
-            params.gravity = Gravity.BOTTOM | Gravity.RIGHT;
+            params.gravity = Gravity.TOP | Gravity.RIGHT;
             mFloatingButton.setLayoutParams(params);
         } else {
             mFloatingButton.setVisibility(View.GONE);
@@ -327,8 +340,12 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
     protected void onAttachedToWindow () {
         super.onAttachedToWindow();
         mFloatingButton = ((View)getParent()).findViewById(R.id.floating_action_button);
-        mFloatingButton.setOnClickListener(new View.OnClickListener() {
+        mClearRecents = ((View)getParent()).findViewById(R.id.clear_recents);
+        mClearRecents.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if (mFloatingButton.getAlpha() != 1f) {
+                    return;
+                }
                 dismissAllTasksAnimated();
             }
         });
