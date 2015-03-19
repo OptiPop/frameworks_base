73c62dbeeed97b6dd0933fd6a9f7d400540befd5
Chet Kener <Cl3Kener@gmail.com>
2015-03-19 14:33:28 -0400

    Revert "[ActivityManager] Invalidate last screenshot when activity relaunched"
    This reverts commit 28b2a272ea1829a322ae2f2656cb34d3b526675c.

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -1791,9 +1791,6 @@ final class ActivityStack {
                     // Do over!
                     mStackSupervisor.scheduleResumeTopActivities();
                 }
-                if (next == mLastScreenshotActivity) {
-                    invalidateLastScreenshot();
-                }
                 if (mStackSupervisor.reportResumedActivityLocked(next)) {
                     mNoAnimActivities.clear();
                     if (DEBUG_STACK) mStackSupervisor.validateTopActivitiesLocked();
