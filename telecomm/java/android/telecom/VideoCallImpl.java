8f104cf7255e275f3905f0de9e8f52a2903e6431
Muhammed Siju <msiju@codeaurora.org>
2015-03-19 21:27:12 +0530

    IMSVT: Fix data usage value.
    Data usage value was always received as 0 due to
    wrong API used for sending Message.
    
    Change-Id: I4626e51f28f64a33fcb6245da9cdbdca36a8215d
    CRs-Fixed: 810409
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- telecomm/java/android/telecom/VideoCallImpl.java
+++ telecomm/java/android/telecom/VideoCallImpl.java
@@ -89,7 +89,7 @@ public class VideoCallImpl extends VideoCall {
 
         @Override
         public void changeCallDataUsage(int dataUsage) {
-            mHandler.obtainMessage(MSG_CHANGE_CALL_DATA_USAGE, dataUsage).sendToTarget();
+            mHandler.obtainMessage(MSG_CHANGE_CALL_DATA_USAGE, dataUsage, 0).sendToTarget();
         }
 
         @Override
