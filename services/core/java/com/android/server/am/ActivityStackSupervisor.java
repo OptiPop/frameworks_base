da0676f71d57184a648d22297164345e80c4f439
Varad Deshmukh <varadd@codeaurora.org>
2015-03-26 19:39:19 -0700

    Trigger GC Delay for subsequent launches.
    Application launches for apps like GMS Music
    and Maps lead to explicit GC's due to Binder
    activity in the SystemServer. This change
    allows delaying the GC for 3 seconds after
    the application is launched. Subsequent launches
    or first relaunches for killed applications
    are also now covered.
    
    Change-Id: I945f19b1363504f4d5f61deb1a3af23149b80e64
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -2713,6 +2713,7 @@ public final class ActivityStackSupervisor implements DisplayListener {
                 }
                 final ActivityRecord ar = stack.findTaskLocked(r);
                 if (ar != null) {
+                    BinderInternal.modifyDelayedGcParams();
                     return ar;
                 }
             }
