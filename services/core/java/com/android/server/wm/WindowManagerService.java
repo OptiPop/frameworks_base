153e191c3fcc20ad5b654a0c3816927d1d39ea49
Chet Kener <Cl3Kener@gmail.com>
2015-03-28 09:00:02 -0400

    Fix Choppy Animations
    * 0.25 causes very choppy animations.  Let's start with .75 which is still fast but doesn't cause loss of framerates. If the users don't mind choppy animations they can use developer menu to change settings.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -626,9 +626,9 @@ public class WindowManagerService extends IWindowManager.Stub
     PowerManager mPowerManager;
     PowerManagerInternal mPowerManagerInternal;
 
-    float mWindowAnimationScaleSetting = 0.25f;
-    float mTransitionAnimationScaleSetting = 0.25f;
-    float mAnimatorDurationScaleSetting = 0.25f;
+    float mWindowAnimationScaleSetting = 0.75f;
+    float mTransitionAnimationScaleSetting = 0.75f;
+    float mAnimatorDurationScaleSetting = 0.75f;
     boolean mAnimationsDisabled = false;
 
     final InputManagerService mInputManager;
