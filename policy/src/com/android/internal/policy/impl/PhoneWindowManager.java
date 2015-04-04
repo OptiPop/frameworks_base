6ab16273d8ecff8f886ee308cfea78a5fe491b9f
Maxime Poulain <maxime.poulain@student.uclouvain.be>
2015-04-04 05:35:31 +0200

    [1/2] KILL_APP_LONGPRESS_BACK: move from secure settings to system settings

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2698,8 +2698,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
             return -1;
         } else if (keyCode == KeyEvent.KEYCODE_BACK) {
-            if (Settings.Secure.getIntForUser(mContext.getContentResolver(),
-                    Settings.Secure.KILL_APP_LONGPRESS_BACK, 0, UserHandle.USER_CURRENT) == 1) {
+            if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.KILL_APP_LONGPRESS_BACK, 0, UserHandle.USER_CURRENT) == 1) {
                 if (down && repeatCount == 0) {
                     mHandler.postDelayed(mBackLongPress, mBackKillTimeout);
                 }
