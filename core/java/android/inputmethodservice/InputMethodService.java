146efbd2f296bb7cf34c68ca709d9534c36d52b2
Kyle Russell <bkylerussell@gmail.com>
2015-05-04 17:07:07 -0400

    InputMethodService: Don't change visibility of fullscreenArea
    Gives apps more flexibility over the input method layout by allowing
    the extractArea and candidatesArea FrameLayouts to be controlled
    independently instead of through the fullscreenArea ViewGroup visibility.
    Otherwise, the candidatesArea may be unnecessarily hidden when the
    extractArea is hidden.
    
    For example, when flagNoExtractUi is specified on an EditText widget,
    then the fullscreenArea ViewGroup is hidden, which makes the
    candidatesArea not shown.  So when in fullscreen mode, the IME
    cannot use the candidatesArea when flagNoExtractUi is specified.  IMEs
    should be able to use the two independently.
    
    In fact, InputMethodService itself recommends using the candidates
    area in fullscreen mode, and provides an API (onDisplayCompletions())
    for apps to know when to modify the candidate area, so it doesn't make
    sense for the candidates area to be restricted by flagNoExtractUi.
    
    Also updates typo in getCandidatesHiddenVisibility() documentation.
    
    Change-Id: Ifd6f54a4654b613f6a95e756c8f8efa02517ffd9
    Signed-off-by: Kyle Russell <bkylerussell@gmail.com>

--- core/java/android/inputmethodservice/InputMethodService.java
+++ core/java/android/inputmethodservice/InputMethodService.java
@@ -925,15 +925,13 @@ public class InputMethodService extends AbstractInputMethodService {
             LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams)
                     mFullscreenArea.getLayoutParams();
             if (isFullscreen) {
-                mFullscreenArea.setBackgroundDrawable(mThemeAttrs.getDrawable(
-                        com.android.internal.R.styleable.InputMethodService_imeFullscreenBackground));
                 lp.height = 0;
                 lp.weight = 1;
             } else {
-                mFullscreenArea.setBackgroundDrawable(null);
                 lp.height = LinearLayout.LayoutParams.WRAP_CONTENT;
                 lp.weight = 0;
             }
+            updateFullscreenBackground(isFullscreen);
             ((ViewGroup)mFullscreenArea.getParent()).updateViewLayout(
                     mFullscreenArea, lp);
             if (isFullscreen) {
@@ -952,6 +950,15 @@ public class InputMethodService extends AbstractInputMethodService {
             onConfigureWindow(mWindow.getWindow(), isFullscreen, !mShowInputRequested);
             mLastShowInputRequested = mShowInputRequested;
         }
+    }
+
+    private void updateFullscreenBackground(boolean shown)
+    {
+        if(shown)
+            mFullscreenArea.setBackgroundDrawable(mThemeAttrs.getDrawable(
+                    com.android.internal.R.styleable.InputMethodService_imeFullscreenBackground));
+        else
+            mFullscreenArea.setBackgroundDrawable(null);
     }
     
     /**
@@ -1015,9 +1022,7 @@ public class InputMethodService extends AbstractInputMethodService {
      * when the input method is in fullscreen mode, and thus showing extracted
      * text.  When false, the extracted text will not be shown, allowing some
      * of the application to be seen behind.  This is normally set for you
-     * by {@link #onUpdateExtractingVisibility}.  This controls the visibility
-     * of both the extracted text and candidate view; the latter since it is
-     * not useful if there is no text to see.
+     * by {@link #onUpdateExtractingVisibility}.
      */
     public void setExtractViewShown(boolean shown) {
         if (mExtractViewHidden == shown) {
@@ -1061,7 +1066,7 @@ public class InputMethodService extends AbstractInputMethodService {
                         this, animRes));
             }
         }
-        mFullscreenArea.setVisibility(vis);
+        updateFullscreenBackground(vis == View.VISIBLE);
     }
     
     /**
@@ -1184,7 +1189,7 @@ public class InputMethodService extends AbstractInputMethodService {
      * or {@link View#GONE View.GONE}) of the candidates view when it is not
      * shown.  The default implementation returns GONE when
      * {@link #isExtractViewShown} returns true,
-     * otherwise VISIBLE.  Be careful if you change this to return GONE in
+     * otherwise INVISIBLE.  Be careful if you change this to return GONE in
      * other situations -- if showing or hiding the candidates view causes
      * your window to resize, this can cause temporary drawing artifacts as
      * the resize takes place.
