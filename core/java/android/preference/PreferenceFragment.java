b012d254b8a9036cb6f5d67af007ba37d047cc99
Chet Kener <Cl3Kener@gmail.com>
2015-05-21 17:30:48 -0400

    Revert "PreferenceFragment : Set title if specified"
    This reverts commit 62eeebedae027d0270e8aacd616eda8badd859e5.
    
    Change-Id: I4f23780964c89ca48e52324b7e69faf763044fe6

--- core/java/android/preference/PreferenceFragment.java
+++ core/java/android/preference/PreferenceFragment.java
@@ -264,11 +264,6 @@ public abstract class PreferenceFragment extends Fragment implements
                 postBindPreferences();
             }
         }
-        CharSequence title = preferenceScreen.getTitle();
-        // Set the title of the activity
-        if (title != null) {
-            getActivity().setTitle(title);
-        }
     }
 
     /**
